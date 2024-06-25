e8e57494554224ab206e9a44d87f1433b4dee84d
Sumit <sumitkhandegar2@gmail.com>
2024-06-25 11:44:18 +0530

    fix:.env-file

--- src/SearchBox.jsx
+++ src/SearchBox.jsx
@@ -8,11 +8,8 @@ export default function SearchBox({ updateInfo }) {
     let [city, setCity] = useState("");
     let [error, setError] = useState(false);
 
-    // const API_URL = import.meta.env.VITE_API_URL;
-    // const API_KEY = import.meta.env.VITE_API_KEY;
-
-    const API_URL = "https://api.openweathermap.org/data/2.5/weather";
-    const API_KEY = "157507500d66b254cda8e6506f059d5b";
+    const API_URL = import.meta.env.VITE_API_URL;
+    const API_KEY = import.meta.env.VITE_API_K;
 
     let getWeatherInfo = async () => {
         try {
